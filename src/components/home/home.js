78d96aff3d310e47fb8fde3f038342da3a59e520
Omar Guerra Alfaro <102171656+omGuerraAlfaro@users.noreply.github.com>
2023-06-08 00:34:59 -0400

    newchanges

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -11,7 +11,7 @@ import Footer from "../footer/footer";
 
 function HomeViewAdmin(props) {
   const [currentPage, setCurrentPage] = useState(0);
-  const elementsPerPage = 15;
+  const elementsPerPage = 3;
   const nombreUser = localStorage.getItem('name_user');
 
   const handleClick = () => {
@@ -21,151 +21,117 @@ function HomeViewAdmin(props) {
     setCurrentPage(selected);
   };
 
-  const data = [
-    {
-        "form_id": 2,
-        "type": "Feature",
-        "image": null,
-        "properties": {
-            "propid": 2,
-            "userId": "Iparra",
-            "dateTime": "2023-01-17T16:26:11.574Z",
-            "formSprinkler": {
-                "spid": 1,
-                "spcode": "sp-1",
-                "defect": "tapado",
-                "repaired": "no",
-                "observation": null
-            },
-            "formDamage": null,
-            "formHumidity": null,
-            "formCompaction": null,
-            "formFauna": null,
-            "formCount": null,
-            "formDiseases": null,
-            "formGirdling": null,
-            "formPlague": null
-        },
-        "geometry": {
-            "gid": 31,
-            "type": "Point",
-            "coordinates": [
-                -71.0509466,
-                -32.8408526
-            ]
-        }
-    }
-]
-;
-
-  // const startIndex = currentPage * elementsPerPage;
-  // const endIndex = startIndex + elementsPerPage;
-  // const displayedData = data.slice(startIndex, endIndex);
   const [tableData, setTableData] = useState([]);
+  const [error, setError] = useState(null);
 
   const fetchTableData = async () => {
     try {
-      const url = 'http://localhost:3000/forms';
+      const url = 'http://localhost:3400/forms';
       const config = {
         headers: {
           'Content-Type': 'application/json',
         },
       };
-  
+
       const response = await axios.get(url, config);
       setTableData(response.data);
-      console.log(response.data);
     } catch (error) {
-      console.log("Error al obtener los datos de la tabla:", error);
+      setError("Error al obtener los datos de la tabla: " + error);
+      console.error(error);
     }
   };
+
   useEffect(() => {
     fetchTableData();
   }, []);
 
+  // calculate displayedData based on current page
+  const displayedData = (tableData || []).slice(currentPage * elementsPerPage, (currentPage * elementsPerPage) + elementsPerPage);
+
   return (
     <div>
-      <Navbar nombreUser={nombreUser}/>
+      <Navbar nombreUser={nombreUser} />
 
       <img src={fondo} className="fondo"></img>
       <div className="container data-table">
-        <div className="container d-flex g-3 justify-content-end">
-          <div className="border rounded p-1">
-            <h6 className="text-center">Filtrar por:</h6>
-            <ButtonFilter
-              clase="mx-1"
-              nombre="Tipo"
-              color="danger"
-              tamaño="sm"
-              onClick={handleClick}
-            />
-            <ButtonFilter
-              clase="mx-1"
-              nombre="Encargado"
-              color="secondary"
-              tamaño="sm"
-              onClick={handleClick}
-            />
-            <ButtonFilter
-              clase="mx-1"
-              nombre="Fecha"
-              color="secondary"
-              tamaño="sm"
-              onClick={handleClick}
-            />
+        <div className="container-rounded">
+          <div className="container d-flex g-3 justify-content-end">
+            <div className="border rounded p-1">
+              <h6 className="text-center">Filtrar por:</h6>
+              <ButtonFilter
+                clase="mx-1"
+                nombre="Tipo"
+                color="danger"
+                tamaño="sm"
+                onClick={handleClick}
+              />
+              <ButtonFilter
+                clase="mx-1"
+                nombre="Encargado"
+                color="secondary"
+                tamaño="sm"
+                onClick={handleClick}
+              />
+              <ButtonFilter
+                clase="mx-1"
+                nombre="Fecha"
+                color="secondary"
+                tamaño="sm"
+                onClick={handleClick}
+              />
+            </div>
           </div>
-        </div>
-        <h1 className="titulo">Lista Tareas</h1>
-        <div class="table-responsive">
-          <div className="scroll">
-            <table className="table table-striped table-responsive">
-              <thead>
-                <tr>
-                  <th scope="col">ID Formulario</th>
-                  <th scope="col">Tipo</th>
-                  <th scope="col">Nombre</th>
-                  <th scope="col">Sector</th>
-                  <th scope="col">Fecha</th>
-                  <th scope="col" class="text-center">
-                    Acciones
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
+          <h1 className="titulo">Lista Tareas</h1>
+          <div class="table-responsive">
+            <div className="scroll">
+              <table className="table table-striped table-responsive">
+                <thead>
+                  <tr>
+                    <th scope="col">ID Formulario</th>
+                    <th scope="col">Tipo</th>
+                    <th scope="col">Fecha y Hora</th>
+                    <th scope="col">Sector</th>
+                    <th scope="col">Tipo Sector</th>
+                    <th scope="col" class="text-center">
+                      Acciones
+                    </th>
+                  </tr>
+                </thead>
                 <tbody>
-                  {tableData.map((item) => (
+                  {displayedData.map((item) => (
                     <tr key={item.id}>
-                      <th scope="row">{item.id}</th>
-                      <td>{item.tipo}</td>
-                      <td>{item.nombre}</td>
-                      <td>{item.sector}</td>
-                      <td>{item.fecha}</td>
+                      <th scope="row">{item.form_id}</th>
+                      <td>{item.type}</td>
+                      <td>{item.properties.dateTime}</td>
+                      <td>{item.geometry.gid}</td>
+                      <td>{item.geometry.type}</td>
                       <td className="text-center">
                         <ButtonG />
                       </td>
                     </tr>
                   ))}
                 </tbody>
-              </tbody>
-            </table>
+              </table>
+              {error && <p>Error: {error}</p>}
+            </div>
           </div>
-        </div>
-        <ReactPaginate
-          previousLabel={"Anterior"}
-          nextLabel={"Siguiente"}
-          breakLabel={"..."}
-          breakClassName={"break-me"}
-          pageCount={Math.ceil(data.length / elementsPerPage)}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
-          onPageChange={handlePageChange}
-          containerClassName={"pagination"}
-          subContainerClassName={"pages pagination"}
-          activeClassName={"active"}
-        />
-      </div>
+          <ReactPaginate
+            previousLabel={"Anterior"}
+            nextLabel={"Siguiente"}
+            breakLabel={"..."}
+            breakClassName={"break-me"}
+            pageCount={Math.ceil((tableData?.length || 0) / elementsPerPage)}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={handlePageChange}
+            containerClassName={"pagination"}
+            subContainerClassName={"pages pagination"}
+            activeClassName={"active"}
+          />
 
-      <Footer />
+        </div>
+      </div>
+        <Footer  className="footer"/>
     </div>
   );
 }
